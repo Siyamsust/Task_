24845954c4ee4ddfe3db646f360c819577534c69
Abir-cse-18 <107100418+Abir-cse-18@users.noreply.github.com>
2025-06-08 20:00:16 +0600

    updated restaurant schema

--- backend/models/Restaurant.js
+++ backend/models/Restaurant.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-
 const restaurantSchema = new mongoose.Schema({
   name: String,
   location: String,
+  exactLocation: String,
+  image: String,
   rating: Number,
-  image: String
+  contact: String,
+  description: String
 });
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);
