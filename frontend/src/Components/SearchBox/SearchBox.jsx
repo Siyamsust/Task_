67de58500537f15de8ec04627a0a85dffa3dc62f
kaoser <akaoser41@gmail.com>
2025-06-10 08:25:51 +0600

    search and filter page complete

--- frontend/src/Components/SearchBox/SearchBox.jsx
+++ frontend/src/Components/SearchBox/SearchBox.jsx
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../HeroSection/HeroSection.css';
+import './SearchBox.css';
+
 const SearchBox = ({ onSearch }) => {
-    const navigate = useNavigate();
-    const [searchTerm, setSearchTerm] = useState('');
-    const [category, setCategory] = useState('');
-    const handleSearch = () => {
-        navigate(`/search?query=${searchTerm}&category=${category}`);
-      };
-    
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSearch = () => {
+    navigate(`/search?query=${searchTerm}&category=${category}`);
+  };
+
   return (
-    <div className="search-box">
-          <i className="fas fa-search"></i>
-          <input 
-            type="text" 
-            placeholder="Search destinations, tours, or activities..."
-            onChange={(e) => {
-              setSearchTerm(e.target.value);
-              onSearch(e.target.value);
-            }}
-          />
-          <button className="search-button" onClick={handleSearch}>Search</button>
-        </div>
+    <div className="custom-search-box">
+      <i className="custom-search-icon fas fa-search"></i>
+      <input
+        type="text"
+        className="custom-search-input"
+        placeholder="Search destinations, tours, or activities..."
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+          onSearch(e.target.value);
+        }}
+      />
+      <button className="custom-search-button" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
   );
 };
 
 export default SearchBox;
-
